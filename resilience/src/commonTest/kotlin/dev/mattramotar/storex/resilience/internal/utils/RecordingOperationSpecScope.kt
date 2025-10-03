d5a3b88bb9dbd5ac1d206ca3b9db602560fb37e8
Matt Ramotar <matt.ramotar@uber.com>
2025-10-02 23:56:30 -0400

    Cover Resilience DSL

--- resilience/src/commonTest/kotlin/dev/mattramotar/storex/resilience/internal/utils/RecordingOperationSpecScope.kt
+++ resilience/src/commonTest/kotlin/dev/mattramotar/storex/resilience/internal/utils/RecordingOperationSpecScope.kt
@@ -4,14 +4,15 @@ import dev.mattramotar.storex.resilience.CircuitBreaker
 import dev.mattramotar.storex.resilience.RetryPolicy
 import dev.mattramotar.storex.resilience.dsl.OperationSpecScope
 import kotlin.test.assertSame
+import kotlin.test.assertTrue
 import kotlin.time.Duration
 
 internal class RecordingOperationSpecScope<T> : OperationSpecScope<T> {
     override var call: (suspend () -> T)? = null
     override var circuitBreaker: CircuitBreaker? = null
     override var timeout: Duration = Duration.ZERO
-    override var retryPolicy: RetryPolicy = RetryPolicy { null }
-    override var retryOn: (Throwable) -> Boolean = { false }
+    override var retryPolicy: RetryPolicy = INITIAL_RETRY_POLICY
+    override var retryOn: (Throwable) -> Boolean = INITIAL_RETRY_ON
     override var failureThreshold: Int = 0
     override var openTTL: Duration = Duration.ZERO
     override var probeQuota: Int = 0
@@ -24,6 +25,11 @@ internal class RecordingOperationSpecScope<T> : OperationSpecScope<T> {
         val current = call ?: error("call {} is required!")
         return current()
     }
+
+    companion object {
+        val INITIAL_RETRY_POLICY: RetryPolicy = RetryPolicy { null }
+        val INITIAL_RETRY_ON: (Throwable) -> Boolean = { false }
+    }
 }
 
 fun <T> assertThat(actual: T) = AssertionSubject(actual)
@@ -35,6 +41,10 @@ class AssertionSubject<T>(private val actual: T) {
 
     fun isSameInstanceAs(expected: T) {
         assertSame(actual, expected)
+    }
+
+    fun isNotSameInstanceAs(expected: T) {
+        assertTrue(actual !== expected)
     }
 
     fun isNotNull() {
